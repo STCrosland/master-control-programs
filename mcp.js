ebec58145090aae8cc71be4aa67ceb430f3d8470
STCrosland <spencertcrosland@gmail.com>
2026-02-03 00:16:33 -0700

    upload rest of site

---
+++ mcp.js
@@ -0,0 +1,8 @@
+// Mobile navigation
+const btn = document.querySelector('.menu-btn');
+const menu = document.querySelector('nav');
+
+btn.addEventListener('click', () => {
+  menu.classList.toggle('open');
+  btn.classList.toggle('change');
+});
